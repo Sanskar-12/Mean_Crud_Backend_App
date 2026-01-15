1b3fc115aa6a19baf7c2c63729af447337a2b816
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-15 12:36:10 +0530

    Feature: Connected server to mongodb

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-
-const app = express();
+import connectToDB from "./config/db.js";
 
 dotenv.config();
+connectToDB();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port: ${process.env.PORT}`);
