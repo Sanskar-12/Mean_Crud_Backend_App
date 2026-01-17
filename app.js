285b1c07689af56e07d808efaacaabad50703162
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-17 17:16:24 +0530

    Feature: Added cors for cross origin calls

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectToDB from "./config/db.js";
 import morgan from "morgan";
 import studentRouter from "./routes/student.routes.js";
+import cors from "cors";
 
 dotenv.config();
 connectToDB();
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello from the server");
